7855cfd1f239a51e62358a1dde60adc513ae8a5f
Silviu Rosu <silviu.rosu@gmail.com>
2019-11-27 22:53:57 +0200

    update deps

--- mix.exs
+++ mix.exs
@@ -2,23 +2,26 @@ defmodule Plugsnag.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :plugsnag,
-     version: "1.4.1",
-     elixir: "~> 1.0",
-     package: package(),
-     description: """
-       Bugsnag reporter for Elixir's Plug
-     """,
-     deps: deps(),
-     dialyzer: [plt_add_deps: :project]
-   ]
+    [
+      app: :plugsnag,
+      version: "1.4.1",
+      elixir: "~> 1.0",
+      package: package(),
+      description: """
+        Bugsnag reporter for Elixir's Plug
+      """,
+      deps: deps(),
+      dialyzer: [plt_add_deps: :project]
+    ]
   end
 
   def package do
-    [contributors: ["Jared Norman", "Andrew Harvey"],
-     maintainers: ["Andrew Harvey"],
-     licenses: ["MIT"],
-     links: %{github: "https://github.com/jarednorman/plugsnag"}]
+    [
+      contributors: ["Jared Norman", "Andrew Harvey"],
+      maintainers: ["Andrew Harvey"],
+      licenses: ["MIT"],
+      links: %{github: "https://github.com/jarednorman/plugsnag"}
+    ]
   end
 
   def application do
@@ -26,10 +29,11 @@ defmodule Plugsnag.Mixfile do
   end
 
   defp deps do
-    [{:bugsnag, "~> 1.3"},
-     {:plug, "~> 1.0"},
-     {:ex_doc, ">= 0.0.0", only: :dev},
-     {:dialyxir, "~> 0.3.5", only: [:dev]}
+    [
+      {:bugsnag, "~> 1.3 or ~> 2.0"},
+      {:plug, "~> 1.0"},
+      {:ex_doc, "~> 0.19", only: :dev},
+      {:dialyxir, "~> 0.3.5", only: [:dev]}
     ]
   end
 end
